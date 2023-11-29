c5e8962720c0f4c75252b93a5a89793cbf40a6ba
81f8 <sllaim333@gmail.com>
2023-11-29 22:01:02 +0300

    cover

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,21 @@
+import Container from "@/component/Container/Container";
 import Header from "@/component/Header/Header";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <>
         <Header />
+        <div className={styles.cover}>
+      <div className={styles.overlay}>
+            <Container>
+              <div className={styles.title}>
+                <h1>Simple Blog</h1>
+                <p>A blog created by aon 2023</p>
+              </div>
+            </Container>
+          </div>
+        </div>
         <div>sssssssss</div>
     </>
   );
